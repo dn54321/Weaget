408eb47ce504fbddb14b77d065d2246c5c587d88
Daniel Pham <daniel_m_pham@hotmail.com>
2024-06-10 17:32:36 +1000

    Added robust logging in development

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    logging: {
+        fetches: {
+            fullUrl: process.env.NODE_ENV === "development",
+        },
+    },
+}
+   
+module.exports = nextConfig
