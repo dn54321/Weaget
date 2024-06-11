7817c87c7967115ccd97b8659ad44872a111c602
Daniel Pham <daniel_m_pham@hotmail.com>
2024-06-11 18:13:45 +1000

    Added basic CSP policy

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-    const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
     const cspHeader = `
       default-src 'self';
       script-src 'self' 'unsafe-inline' 'unsafe-eval';
@@ -19,23 +18,18 @@ export function middleware(request: NextRequest) {
       .replace(/\s{2,}/g, ' ')
       .trim()
    
-    const requestHeaders = new Headers(request.headers)
-    requestHeaders.set(
-      'Content-Security-Policy',
-      contentSecurityPolicyHeaderValue
-    )
-   
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+
     const response = NextResponse.next({
       request: {
         headers: requestHeaders,
       },
-    })
-    response.headers.set(
-      'Content-Security-Policy',
-      contentSecurityPolicyHeaderValue
-    )
+    });
+
+    response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
    
-    return response
+    return response;
 }
 
 export const config = {
