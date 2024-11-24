2ceca33dd3ab94abf7295f26a67bc7b3a2dbd174
Daniel Pham <daniel_m_pham@hotmail.com>
2024-11-24 20:03:15 +1100

    Updated packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,14 @@ import { CssBaseline } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "three-dots/dist/three-dots.css";
 import "@styles/globals.css";
 import { queryClient } from "@utils/query-client";
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import { SystemThemeProvider } from "@components/provider/system-theme-provider";
 import { SettingsStoreProvider } from "@components/provider/settings-provider";
 import WidgetStoreProvider from "@components/provider/widget-provider/widget-store-provider.component";
-import "@mui/material-pigment-css/styles.css";
 import "@src/i18n/i18n";
 // We can't add this metadata until nextjs supports emotion on the server side.
 // https://nextjs.org/docs/app/building-your-application/styling/css-in-js
@@ -33,6 +32,9 @@ import "@src/i18n/i18n";
 // };
 
 export default function RootLayout(props: { children: React.ReactNode }) {
+    inject();
+    injectSpeedInsights();
+
     return (
         <html lang="en">
             <head>
@@ -48,8 +50,6 @@ export default function RootLayout(props: { children: React.ReactNode }) {
                                 <QueryClientProvider client={queryClient}>
                                     {props.children}
                                     <ReactQueryDevtools buttonPosition="bottom-left" />
-                                    <Analytics />
-                                    <SpeedInsights />
                                 </QueryClientProvider>
                             </SystemThemeProvider>
                         </WidgetStoreProvider>
