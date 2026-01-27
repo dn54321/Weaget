747fe7e56818571e60635f4fe9ec2cf310f1ad44
Daniel Pham <daniel_m_pham@hotmail.com>
2026-01-27 15:30:59 +1100

    checkpoint

--- src/hooks/use-get-weather.ts
+++ src/hooks/use-get-weather.ts
@@ -3,6 +3,14 @@ import { OneCallWeatherDetails } from "@features/open-weather-map-one-call/oneCa
 import { useQuery } from "@tanstack/react-query";
 import weatherSchema from "@features/weaget/weather/weather.schema";
 
+/**
+ * Fetches weather data from the API.
+ * 
+ * @param location The location for which to fetch weather data.
+ * @param region The region to narrow down the location search.
+ * @param lang  The language for the weather data.
+ * @returns Weather details.
+ */
 async function fetchWeather(location?: string, region?: string, lang?: string) {
     const url = `/api/weather/${location}`;
     const queryParams = new URLSearchParams({
@@ -18,11 +26,20 @@ async function fetchWeather(location?: string, region?: string, lang?: string) {
         .then(data => weatherSchema.parse(data));
 }
 
+/**
+ * Fetches weather data using React Query.
+ * 
+ * @param location The location for which to fetch weather data.
+ * @param region The region to narrow down the location search.
+ * @param lang The language for the weather data.
+ * @returns Weather details wrapped in a React Query object.
+ */
 export function useGetWeather(location?: string, region?: string, lang?: string) {
     return useQuery<OneCallWeatherDetails>({
         enabled: Boolean(location),
         queryFn: () => fetchWeather(location, region, lang),
         queryKey: ["weather", location, { lang, region }],
         retry: 0,
+        gcTime: 30 * 60 * 1000,
     });
 }
